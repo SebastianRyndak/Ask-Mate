435764fc09e8e873b0ee514191137c722bfe242c
Witold Brzezinski <witbrzezinski@gmail.com>
2021-11-16 10:59:48 +0100

    added some reading operations in connection.py

---
+++ connection.py
@@ -0,0 +1,34 @@
+import csv
+import time
+import os
+
+ANSWER_DATA_PATH = os.getenv("ANSWER_DATA_PATH") if "ANSWER_DATA_PATH" in os.environ else "sample_data/answer.csv"
+ANSWER_DATA_HEADERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+
+
+def get_all_answers():
+    answers_list = []
+    with open(ANSWER_DATA_PATH, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            answers_list.append(row)
+    return answers_list
+
+
+def get_answer_by_id(id):
+    answer_dict = {}
+    with open(ANSWER_DATA_PATH, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['id'] == id:
+                answer_dict = row
+    return answer_dict
+
+
+def get_max_answer_id():
+    id_list = []
+    with open(ANSWER_DATA_PATH, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            id_list.append(row['id'])
+    return max(id_list)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/question/<question_id>/new-answer", methods=["POST", "GET"])
+def add_new_answer(question_id):
+    return render_template("new_answer.html")
 
 
 if __name__ == "__main__":
