f606668d76d786ad7a6a44892f8700dea4d64a50
BartoszKosicki <kosickibartek@gmail.com>
2021-11-16 12:07:14 +0100

    add import module and headers in data manager

---
+++ data_manager.py
@@ -0,0 +1,5 @@
+
+QUESTION_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+ANSWER_HEADERS = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+TABLE_HEADERS = ["Vote number", "title", "message", "image"]
+
