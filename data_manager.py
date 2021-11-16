17bb3e70589b0b497216ab5e95c2fc437751e52c
BartoszKosicki <kosickibartek@gmail.com>
2021-11-16 22:48:03 +0100

    sort question table content

--- data_manager.py
+++ data_manager.py
@@ -4,12 +4,28 @@ import connection
 QUESTION_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
 ANSWER_HEADERS = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
 TABLE_HEADERS = {"vote_number": "Votes", "title": "Title", "message": "Message", "view_number": "Views"}
+SORT_BY_INT = ["vote_number", "view_number"]
+reverse = 0
 
 
-def dictionaries_sort(data, sort_value="submission_time", reverse=True):
-    return sorted(data, key=itemgetter(sort_value), reverse=reverse)
-
-
-def prepare_table_to_display():
+def prepare_table_to_display(descend=0, sort_value="submission_time"):
     data = connection.import_data("./sample_data/question.csv")
-    return dictionaries_sort(data), TABLE_HEADERS
+    return dictionaries_sort(data, descend, sort_value), TABLE_HEADERS
+
+
+def switch_value_type(data, type):
+    for dic in data:
+        for key, value in dic.items():
+            if key in SORT_BY_INT:
+                dic[key] = type(value)
+    return data
+
+
+def dictionaries_sort(data, descend, sort_value):
+    global reverse
+    reverse = (reverse + int(descend)) % 2
+    if sort_value in SORT_BY_INT:
+        data = switch_value_type(data, int)
+        sorted_data = sorted(data, key=itemgetter(sort_value), reverse=reverse)
+        return switch_value_type(sorted_data, str)
+    return sorted(data, key=itemgetter(sort_value), reverse=reverse)
--- server.py
+++ server.py
@@ -3,6 +3,12 @@ import data_manager
 
 app = Flask(__name__)
 
+@app.route("/<value>/<descend>")
+def prepare_sorted_table_to_display(descend, value):
+    questions_list, table_headers = data_manager.prepare_table_to_display(int(descend), value)
+    return render_template("list.html", questions_list=questions_list, table_headers=table_headers)
+
+
 @app.route("/")
 @app.route("/list")
 def question_list():
