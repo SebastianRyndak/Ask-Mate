ed885b3202dc9736c830f5222cb3e8b647e6d069
Witold Brzezinski <witbrzezinski@gmail.com>
2021-11-16 14:35:42 +0100

    adding answer to data works

--- data_manager.py
+++ data_manager.py
@@ -4,5 +4,7 @@ import time
 
 def add_new_answer(question_id, message, image):
     answer_id = int(connection.get_max_answer_id()) + 1
-    submission_time = time.time()
+    submission_time = int(time.time())
     connection.write_answer_to_csv(answer_id, submission_time, 0, question_id, message, image)
+
+#add_new_answer(5,"test","")
--- server.py
+++ server.py
@@ -11,12 +11,25 @@ def hello():
 
 @app.route("/question/<question_id>/new-answer", methods=["POST", "GET"])
 def add_new_answer(question_id):
-    if request.method == 'POST':
-        if request.form.action == 'Post':
-            message = request.form.get("message")
-            data_manager.add_new_answer(question_id, message, "")
-    return render_template("new_answer.html")
+    if request.method == "POST":
+       # if request.form.get("add_na"):
+    #    message = request.form.get("message")
+    #    data_manager.add_new_answer(int(question_id), message, "")
+    #    elif request.form.get("upload_na"):
+    #        print("test")
+    return render_template("new_answer.html", question_id=question_id)
+
+
+@app.route("/question/<question_id>/", methods=["POST", "GET"])
+def list_new_answer(question_id):
+    if request.method == "POST":
+       # if request.form.get("add_na"):
+        message = request.form.get("message")
+        data_manager.add_new_answer(int(question_id), message, "")
+    #    elif request.form.get("upload_na"):
+    #        print("test")
+    return render_template("new_answer.html", question_id=question_id)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
