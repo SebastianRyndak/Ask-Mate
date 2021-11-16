b7bbdf4a1460ec00bd0ba5e99216b41e7f9b6325
Sebastian Ryndak <ryndaksebastiann@gmail.com>
2021-11-16 10:26:16 +0100

    Add-questions v.01

--- server.py
+++ server.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import time
+
 
 app = Flask(__name__)
+ID = 0
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/add-question")
+def add():
+    return render_template("add-question.html")
+
+
+@app.route("/add-question", methods=['POST'])
+def add_question():
+    global ID
+    ID += 1
+    unix_time = int(time.time())
+    title = request.form["title"]
+    question = request.form["question"]
+    image = request.form["image"]
+    dic = {"ID": ID, "Time": unix_time, "Title": title, "Question": question, "Image": image}
+    print(dic)
+    if image is not None:
+        with open("images.txt", "w") as file:
+            file.write(image)
+    return render_template("add-question.html", dic=dic)
 
 
 if __name__ == "__main__":
