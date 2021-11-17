c869644dba96e5fd78b1543c0bfaab3a99f68a42
Sebastian Ryndak <ryndaksebastiann@gmail.com>
2021-11-17 09:59:08 +0100

    Add-questions v.02

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import time
-
+import os
 
 app = Flask(__name__)
 ID = 0
+pictures = ".\\static\\uploads_picture"
+app.config["UPLOAD_PICTURE_FOLDER"] = pictures
 
 
 @app.route("/")
@@ -12,26 +14,22 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/add-question")
-def add():
+@app.route("/add-question", methods=['GET', 'POST'])
+def add_information_about_question():
+    if request.method == "POST":
+        global ID
+        ID += 1
+        unix_time = int(time.time())
+        title = request.form["title"]
+        question = request.form["question"]
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["UPLOAD_PICTURE_FOLDER"], image.filename))
+            dic = {"id": ID, "submission_time": unix_time, "view_number": 0, "vote_number": 0, "title": title, "message": question, "Image": "./static/uploads_picture/" + image.filename}
+            return render_template("index.html")
+
     return render_template("add-question.html")
 
 
-@app.route("/add-question", methods=['POST'])
-def add_question():
-    global ID
-    ID += 1
-    unix_time = int(time.time())
-    title = request.form["title"]
-    question = request.form["question"]
-    image = request.form["image"]
-    dic = {"ID": ID, "Time": unix_time, "Title": title, "Question": question, "Image": image}
-    print(dic)
-    if image is not None:
-        with open("images.txt", "w") as file:
-            file.write(image)
-    return render_template("add-question.html", dic=dic)
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
